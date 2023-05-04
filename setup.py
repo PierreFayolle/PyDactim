e863264e2ed07f02eafb7ae07f44380bf2ffcf6c
PierreFayolle <46927344+PierreFayolle@users.noreply.github.com>
2023-05-04 18:29:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
-install_requires=[
-    "torch>=1.13.1",
-    "torchio>=0.18.86",
-    "SimpleITK",
-    "numpy",
-    "nibabel",
-    "matplotlib",
-    "pydicom>=2.3.1",
-    "dicom2nifti"
-]
+
+from setuptools import setup
+
+setup(name='Dactim_MRI',
+      python_requires='>=3.7',
+      install_requires=[
+        "torch>=1.13.1",
+        "torchio>=0.18.86",
+        "SimpleITK",
+        "numpy",
+        "nibabel",
+        "matplotlib",
+        "pydicom>=2.3.1",
+        "dicom2nifti"
+      ],
+      classifiers=[
+          'Intended Audience :: Science/Research',
+          'Programming Language :: Python',
+          'Topic :: Scientific/Engineering',
+          'Operating System :: Unix'
+      ]
+)
