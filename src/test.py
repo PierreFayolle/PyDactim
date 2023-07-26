816d12590a7f5ee77a8aad7dd97a8723195d149e
Pierre Fayolle <46927344+PierreFayolle@users.noreply.github.com>
2023-07-26 18:34:35 +0200

    Delete src directory

--- src/test.py
+++
@@ -1,5 +0,0 @@
-from dactim_mri.sorting import sort_dicom
-from dactim_mri.conversion import convert_dicom_to_nifti
-from dactim_mri.transformation import resample, registration
-
-registration(r"C:\Users\467355\Desktop\data\Glioma\2_3d_flair_brain.nii.gz", r"C:\Users\467355\Desktop\data\Glioma\3_3d_flair_brain_resampled.nii.gz")
